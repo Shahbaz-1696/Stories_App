b0445cb34a2831c6aff36ffd461bfed7e492bf5a
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-10-24 22:47:36 +0530

    Updated Main page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import { AuthLayout } from "./components/index.js";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 import "./index.css";
 import {store} from "./store/store.js";
 import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AddPost from "./pages/AddPost.jsx";
+import Allposts from "./pages/Allposts.jsx";
+import EditPost from "./pages/EditPost.jsx";
+import Post from "./pages/Post.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/add-post',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <Allposts />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/edit-post/:slug',
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/post/:slug',
+        element: <Post />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
